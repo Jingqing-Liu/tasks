68a147e01735f5a312220d2bfa825f5e933decf8
Jingqing Liu <jingqing@Jingqings-MacBook-Pro.local>
2022-03-07 18:52:01 -0500

    completed ChooseTeam

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -11,21 +11,16 @@ const PEOPLE = [
 ];
 
 export function ChooseTeam(): JSX.Element {
-    const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
     const [team, setTeam] = useState<string[]>([]);
 
-    function chooseMember() {
-        /*
-        if (!team.includes(newMember)) {
-            team.push(newMember);
+    function chooseMember(name: string) {
+        if (!team.includes(name)) {
+            setTeam([...team, name]);
         }
-        */
     }
 
     function clearTeam() {
-        /*
-        team = [];
-        */
+        return setTeam([]);
     }
 
     return (
@@ -33,10 +28,13 @@ export function ChooseTeam(): JSX.Element {
             <h3>Choose Team</h3>
             <Row>
                 <Col>
-                    {allOptions.map((option: string) => (
+                    {PEOPLE.map((option: string) => (
                         <div key={option} style={{ marginBottom: "4px" }}>
                             Add{" "}
-                            <Button onClick={chooseMember} size="sm">
+                            <Button
+                                onClick={() => chooseMember(option)}
+                                size="sm"
+                            >
                                 {option}
                             </Button>
                         </div>
