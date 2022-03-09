e9204962bf4e68cd0846cdc871e657db73094204
Jingqing Liu <jingqing@wifi-roaming-128-4-132-218.wifi.udel.edu>
2022-03-09 10:59:11 -0500

    completed CheckAnswer

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+
+    function updateAnswer(event: ChangeEvent) {
+        setAnswer(event.target.value);
+    }
+
     return (
         <div>
+            <Form.Control value={answer} onChange={updateAnswer} />
             <h3>Check Answer</h3>
+            <div>{answer === expectedAnswer ? "✔️" : "❌"}. </div>
         </div>
     );
 }
