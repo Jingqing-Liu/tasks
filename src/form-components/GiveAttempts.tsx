17084679be01b889bcca2481ff4c1a09e3f3d884
Jingqing Liu <jingqing@Jingqings-MacBook-Pro.local>
2022-03-09 18:23:37 -0500

    completed GiveAttempts

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,42 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 
 export function GiveAttempts(): JSX.Element {
+    const [lefttempts, setLeftattempts] = useState<number>(3);
+    const [newattempts, setNewattempts] = useState<number>(0);
+    const reduceAttempts = () => setLeftattempts(lefttempts - 1);
+    const makegain = () => setLeftattempts(newattempts + lefttempts);
+
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <div>
+                <h3>Give Attempts</h3>
+                <Form.Control
+                    type="number"
+                    value={newattempts}
+                    onChange={(event: ChangeEvent) =>
+                        setNewattempts(parseInt(event.target.value))
+                    }
+                />
+            </div>
+            <div>
+                <span>
+                    <Button
+                        onClick={reduceAttempts}
+                        disabled={lefttempts === 0}
+                    >
+                        use
+                    </Button>
+                </span>
+                <span>
+                    <Button onClick={makegain}>gain</Button>
+                </span>
+            </div>
+            <div>{lefttempts}</div>
         </div>
     );
 }
